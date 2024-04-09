961ea5a6da389e74fc66e9aab010493087dcc907
KaitlynU <kaiturano@gmail.com>
2024-04-08 20:09:34 -0500

    first commit

---
+++ wallflowerApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WallflowerappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wallflowerApp'
---
+++ wallflowerApp/views.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.shortcuts import render
+
+from . import views
+
+def index(request):
+    return render(request, "index.html")
